e175b6ad464c06c167a035c0c2ac65297042731c
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-12-23 22:05:08 -0300

    Se actualiza ejecucion.groovy

--- vars/util.groovy
+++ vars/util.groovy
@@ -16,7 +16,7 @@ def validStages(pipeline_type) {
     return valid_stages
 }
 
-def validateStages(valid_stages  as String[], stages  as String[]){
+def validateStages((String[]) valid_stages, stages){
     println ">>>Valid Stages: ${valid_stages}"
 
     if(stages.trim() == ''){
