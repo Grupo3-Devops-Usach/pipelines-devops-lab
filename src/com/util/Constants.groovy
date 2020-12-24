e19f13bb4aaa4da16df43711ff0b89f2ffd89983
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-12-23 22:57:02 -0300

    Se actualizan archivos .groovy

--- src/com/util/Constants.groovy
+++ src/com/util/Constants.groovy
@@ -4,7 +4,7 @@ class Constants {
     public static final String IC = 'IC'
     public static final String RELEASE = 'RELEASE'
     public static final String MAVEN = 'MAVEN'
-    public static final String GRADLE = 'MAVEN'
+    public static final String GRADLE = 'GRADLE'
     public static final String STAGE_COMPILE = 'compile'
     public static final IC_STAGES = [STAGE_COMPILE, 'unitTest', 'jar', 'sonar', 'nexusUpload', 'gitCreateRelease']
     public static final IC_RELEASE = ['gitDiff', 'nexusDownload', 'run', 'test', 'gitMergeMaster', 'gitMergeDevelop', 'gitTagMaster']
