0798402714b90fa437dd40f69106646a7fb003f2
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-12-27 14:28:52 -0300

    Se actualizan archivos .groovy

--- vars/pipelines.groovy
+++ vars/pipelines.groovy
@@ -1,11 +1,11 @@
 import com.util.Constants
 
-def execute(stages){
-    def iStages = stages.split(Constants.SPLIT_SYMBOL)
+def execute(stages_list){
+    def iStages = stages_list.split(Constants.SPLIT_SYMBOL)
     def valid_stages = util.validStages(env.PIPELINE_TYPE)
 
     for(String value in valid_stages){
-        if(stages.trim() == '' || iStages.contains(value)){
+        if(stages_list.trim() == '' || iStages.contains(value)){
             env.STG_NAME = value
             stages.execute(value)
         }
