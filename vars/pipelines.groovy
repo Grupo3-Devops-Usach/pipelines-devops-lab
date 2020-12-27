70b5e7c6a477ac8fe0371b15e30de1ee45c7eccf
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-12-27 13:38:38 -0300

    Se actualizan archivos .groovy

--- vars/pipelines.groovy
+++ vars/pipelines.groovy
@@ -3,6 +3,9 @@ import com.util.Constants
 def execute(valid_stages, stages){
     def iStages = stages.split(Constants.SPLIT_SYMBOL);
 
+    println "${valid_stages}"
+    println "${stages}"
+
     for(String value in valid_stages){
         if(stages.trim() == '' || iStages.contains(value)){
             env.STG_NAME = value
