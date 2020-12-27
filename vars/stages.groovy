b7aa3b262aaa5a3fa29e9af90bfa472380b8872f
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-12-27 15:05:01 -0300

    Se actualizan archivos .groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -25,6 +25,11 @@ def call(){
                 }
             }
             break
+        case Constants.STAGE_NEXUSUPLOAD:
+            stage(Constants.STAGE_NEXUSUPLOAD){
+                nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'build\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.0']]]
+            }
+            break
         default:
             break
     }
