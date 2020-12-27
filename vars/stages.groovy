b6cef9bd6bc5864e81e28e12074984a392e2b03e
LAPTOP-776AMTE0\carlo.figueroa <carlo.figueroa@sistemassa.cl>
2020-12-27 14:57:53 -0300

    Se actualizan archivos .groovy

--- vars/stages.groovy
+++ vars/stages.groovy
@@ -17,6 +17,14 @@ def call(){
                 bat 'mvnw.cmd clean package -e'
             }
             break
+        case Constants.STAGE_SONAR:
+            stage(Constants.STAGE_SONAR){
+                def scannerHome = tool 'sonar-scanner';
+                withSonarQubeEnv('sonar') {
+                    bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.login=75a0e9b0613f563c0e69a23174cf79eb5d4d74c7"
+                }
+            }
+            break
         default:
             break
     }
