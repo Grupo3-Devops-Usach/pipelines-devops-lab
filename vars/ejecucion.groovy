4033e09002031bae6c0ae24d987ddc05aebc5f03
Albert Munoz <albert.munozp@gmail.com>
2021-01-08 20:23:18 -0300

    fix merge

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,48 +1,4 @@
-<<<<<<< HEAD
-import pipeline.*
 
-def call(){
-
-    pipeline {
-    agent any
-
-    parameters{
-        choice(name: 'buildtool', choices: ['gradle','maven'], description: 'Elección de herramienta de construcción para aplicación covid')
-        string(name: 'stages', defaultValue:'' , description:'Escribir stages a ejecutar en formato: stage1; stage2; stage3. Si stage es vacío, se ejeutarán los stages')
-    }
-
-
-    stages {
-        stage('Pipeline') {
-            steps{
-                script{
-                    env.PIPELINE_TYPE = ''
-                    env.PATHJAR= ''
-                    env.VERSIONCICD= ''
-
-                    def utils =  new test.UtilMethods()
-                    env.PIPELINE_TYPE = utils.pipelineType(env.BRANCH_NAME)
-                    utils.setVaribales(env.PIPELINE_TYPE)
-
-                    println 'Herramienta de ejecución seleccionada: '+ params.buildtool + ' '+ env.BRANCH_NAME
-
-                    if(params.buildtool == 'gradle'){
-                            gradle "${params.stages}" , env.PIPELINE_TYPE
-                        }else{
-                            maven "${params.stages}" , env.PIPELINE_TYPE
-                        }
-                    }
-                }
-            }
-
-        }   
-
-    }
-
-}
-
-return this;
-=======
 def call(){
     pipeline {
         agent any
@@ -98,4 +54,3 @@ def call(){
 }
 
 return this;
->>>>>>> fetature-inicial
